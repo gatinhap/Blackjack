68f5b16cf580c6896582d49e4d903e51a8dd5982
julia-piorek <julia.piorek@webproproductions.com>
2023-01-05 17:14:49 +0100

    game completed

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-let firstCard = 5
-let secondCard = 2
-let cards = [firstCard, secondCard]
+//-------------------------------------------------------------
+// **OBJECT PRACTICE**
+// let player = {
+//     name: "Julia",
+//     chips: 100
+// }
+//
+// let playerEl = document.getElementById('player-el')
+// playerEl.textContent = player.name + ": " + "$" + player.chips
+
+//**code commented out so that anyone who plays the game, doesn't see the hardcoded values**
+//-------------------------------------------------------------
+
+let cards = []
 let cardsEl = document.getElementById('cards-el')
 
-let sum = firstCard + secondCard
+let sum = 0
 let sumEl = document.getElementById('sum-el')
 
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 
 let message = "";
 let messageEl = document.getElementById('message-el')
 
+function getRandomCard() {
+    let randomCard = Math.floor(Math.random() * 13) + 1
+    if (randomCard > 10) {
+        return 10
+    } else if (randomCard === 1) {
+        return 11
+    } else {
+        return randomCard
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
+
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?";
@@ -25,17 +69,6 @@ function renderGame() {
         isAlive = false
     }
     messageEl.textContent = message
-}
-
-function startGame() {
-    renderGame()
-}
-
-function newCard() {
-    let card = 3
-    sum += card
-    cards.push(card)
-    renderGame()
 }
 
 
